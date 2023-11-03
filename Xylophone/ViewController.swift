0336ff71f861facc283bbd3fd612bd4a2cc5bcc0
Francisco Souza <osf.fjose@creditas.com>
2023-11-03 16:30:32 -0300

    feat: add sound play

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -6,14 +6,45 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    private var audioPlayer: AVAudioPlayer?
+    
+    private let audioSession = AVAudioSession.sharedInstance()
+    
+    private let soundTagMapper = [
+        0: "C",
+        1: "D",
+        2: "E",
+        3: "F",
+        4: "G",
+        5: "A",
+        6: "B"
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func onKeyPress(_ sender: UIButton) {
+        guard let note = soundTagMapper[sender.tag] else { return }
+        playSound(note: note)
+    }
+    
+    func playSound(note: String) {
+        guard let url = Bundle.main.url(forResource: note, withExtension: "wav") else { return }
+        
+        do {
+            try audioSession.setCategory(.playback, mode: .default)
+            try audioSession.setActive(true)
+            
+            audioPlayer = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+            audioPlayer?.play()
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
 }
 
